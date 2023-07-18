611704b4a451415a47e452c5ca14ab919ed31bcd
Dev no comando <139907969+devnocomando@users.noreply.github.com>
2023-07-18 15:53:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ function App() {
   return (
     <>
      <img className={styles.pdfContainer} src="./copy.svg" />
+     <div className={styles.absoluteBoxBuy}>
+        <button onClick={() => {
+          window.location.href = 'https://hotm.art/zuSCugl1'
+        }} className={styles.button}>COMPRAR</button>
+     </div>
     </>
   )
 }
