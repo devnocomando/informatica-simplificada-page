4f1f9d5b44d949b964449f675cbf036ee8d62630
Vinicius Gomes <vinigomes.0311@gmail.com>
2023-07-18 14:57:58 -0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+    <>
+     <img className={styles.pdfContainer} src="./copy.svg" />
+    </>
+  )
+}
+
+export default App
