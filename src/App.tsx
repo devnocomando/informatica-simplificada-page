075a8446f02180114bed639739286bf649f7928e
Dev no comando <139907969+devnocomando@users.noreply.github.com>
2023-07-18 20:26:10 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
      <img className={styles.pdfContainer} src="./copy.svg" />
      <div className={styles.absoluteBoxBuy}>
         <button onClick={() => {
-          window.location.href = 'https://hotm.art/zuSCugl1'
+          // window.location.href = 'https://hotm.art/zuSCugl1'
+          window.location.href = 'https://pay.hotmart.com/J84635582W'
         }} className={styles.button}>COMPRAR</button>
      </div>
     </>
